fea6e4a6dab30554125d6d38c3bf2b38129fc6ce
ctiller15 <christiller2015@gmail.com>
2017-12-12 14:58:27 -0500

    Fetch OpenWeather api data

--- src/index.js
+++ src/index.js
@@ -24,6 +24,38 @@ class WeatherApp extends React.Component {
 		}
 	}
 
+	componentDidMount() {
+		fetch('https://api.openweathermap.org/data/2.5/forecast?q=Tampa&appid=c753b4feae5388aca46414f0ab6a4c14').then(function(response){
+			return response.json();
+		}).then((data) => {
+			// At this point, all of the data is logged.
+			var dataObj = {};
+			var dates = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+			// console.log(data.list);
+			var current="";
+			var previous="";
+			data.list.forEach((arr, index) => {
+				let day = new Date(arr.dt*1000);
+				// console.log(day, dates[day.getDay()]);
+				if(!(dates[day.getDay()] in dataObj)){
+					previous = current;
+					current = dates[day.getDay()];
+					// Initialize the array
+					dataObj[current] = [];
+					// Push in the last datapoint for the previous set.
+					if(previous){
+						dataObj[previous].push(arr);				
+					}
+					// push in the first datapoint for the new set.
+					dataObj[current].push(arr);
+				} else if((dates[day.getDay()] in dataObj)){
+					dataObj[dates[day.getDay()]].push(arr);
+				}
+			});
+			this.setState({weatherData: dataObj});
+		});		
+	}
+
 	// Modifies the state of the hourly forecast.
 	changeState(ind) {
 		this.setState({hourly: WeatherData[ind].hourly});
